56ba1efcd4e6cc1a4d9d879c5ccbc0ad6f6a7ca4
stsouko <nougmanoff@protonmail.com>
2020-02-24 06:27:28 +0300

    cli update

--- AFIRdb/__main__.py
+++ AFIRdb/__main__.py
@@ -17,30 +17,52 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with this program; if not, see <https://www.gnu.org/licenses/>.
 #
-from argparse import ArgumentParser, FileType
+from argparse import ArgumentParser, FileType, ArgumentDefaultsHelpFormatter
 from CGRdb import load_schema
 from neomodel import config
 from urllib.parse import urlparse
 from .populate import load_data
+from .wui import dash
 
-parser = ArgumentParser(description="fill DB with data")
 
-parser.add_argument('--eq_file', '-eq', type=FileType(), required=True, help='the file with equilibrium states')
-parser.add_argument('--ts_file', '-ts', type=FileType(), help='the file with transition states')
-parser.add_argument('--pt_file', '-pt', type=FileType(), help='the file with scan pathways')
+def populate_core(args):
+    if not args.ts_file and not args.pt_file:
+        print('At least ts_file or pt_file required')
+    else:
+        load_data(args.eq_file, args.ts_file, args.pt_file)
 
+
+def web_core(args):
+    ds = args.listening
+    dash.run_server(port=ds.port, host=ds.hostname, debug=args.debug)
+
+
+parser = ArgumentParser(description="fill DB with data", formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument('--postgres', '-pg', type=urlparse, required=True,
                     help='postgres connection URL [//user:pass@host:port/schema]')
 parser.add_argument('--neo4j', '-nj', type=str, required=True, help='neo4j connection URL')
 
-args = parser.parse_args()
+subparsers = parser.add_subparsers(title='subcommands', description='available utilities')
 
-if not args.ts_file and not args.pt_file:
-    print('At least ts_file or pt_file required')
-else:
+populate = subparsers.add_parser('populate', help='load data into DB')
+populate.add_argument('--eq_file', '-eq', type=FileType(), required=True, help='the file with equilibrium states')
+populate.add_argument('--ts_file', '-ts', type=FileType(), help='the file with transition states')
+populate.add_argument('--pt_file', '-pt', type=FileType(), help='the file with scan pathways')
+populate.set_defaults(func=populate_core)
+
+web = subparsers.add_parser('wui', help='run WEB UI', formatter_class=ArgumentDefaultsHelpFormatter)
+web.add_argument('--listening', '-ls', type=urlparse,
+                 help='listening host and port [//host:port]', default='//localhost:5000')
+web.add_argument('--debug', action='store_true')
+web.set_defaults(func=web_core)
+
+parsed = parser.parse_args()
+if 'func' in parsed:
     # setup connections
-    pg = args.postgres
+    pg = parsed.postgres
     load_schema(pg.path[1:], password=pg.password, port=pg.port, host=pg.hostname, user=pg.username)
-    config.DATABASE_URL = args.neo4j
-
-    load_data(args.eq_file, args.ts_file, args.pt_file)
+    config.DATABASE_URL = parsed.neo4j
+    # run utility
+    parsed.func(parsed)
+else:
+    parser.print_help()
